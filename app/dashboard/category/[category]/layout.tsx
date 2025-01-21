d0c8e003c9856934fffb7557fd831bcf45500fc0
Archit Agarwal <127117944+ArchitAgarwal04@users.noreply.github.com>
2025-01-21 16:49:38 +0530

    Delete app/dashboard directory

--- app/dashboard/category/[category]/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function CategoryLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return <div className="flex-1 bg-neutral-900">{children}</div>
-  }
-  
-  
